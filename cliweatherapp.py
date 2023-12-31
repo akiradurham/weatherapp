0bb7faffa35e47951dd50e974461bd466026f10a
akira <zup9su@virginia.edu>
2023-12-31 16:09:12 +0900

    refactoring functions

--- cliweatherapp.py
+++ cliweatherapp.py
@@ -1,7 +1,5 @@
-import urllib
 import openmeteo_requests
 import requests_cache
-import pandas as pd
 from retry_requests import retry
 from urllib.request import urlopen
 import json
@@ -14,7 +12,7 @@ def search_location():
         cities = json.loads(urlopen(city_url).read())
         latitude = cities['results'][0]['latitude']
         longitude = cities['results'][0]['longitude']
-        country = cities['results'][0]['country']
+        # country = cities['results'][0]['country']
 
         url = "https://api.open-meteo.com/v1/forecast"
         params = {
@@ -30,30 +28,38 @@ def search_location():
         print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
         print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
     except Exception as e:
+        print(e.__repr__())
         print("Input is not valid location")
+
+
+def get_input():
+    user_input = input("What city's weather do you want?\nEnter in form: City, State/Province, Country\n")
+    return user_input.split(", ")
+
+
+def validate_input(entered):
+    while len(entered) > 3 or len(entered) <= 0:
+        print("Input is not valid, ensure to follow input guidelines")
+        entered = get_input()
+    if len(entered) == 1:
+        print("Defaulting to largest population with city name match ...")
+        input_city = entered[0].replace(' ', '+')
+        search_location()
+    elif len(entered) == 2:
+        print("Matching largest population city and within the state/province ...")
+        input_city = entered[0].replace(' ', '+')
+        input_state = entered[1].replace(' ', '+')
+        search_location()
+    else:
+        print("Matching all city name, state/province, and country ...")
+        input_city = entered[0].replace(' ', '+')
+        input_state = entered[1].replace(' ', '+')
+        input_country = entered[2].replace(' ', '+')
+        search_location()
 
 
 cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 open_meteo = openmeteo_requests.Client(session=retry_session)
 
-user_input = input("What city's weather do you want?\nEnter in form: City, State/Province, Country\n")
-entered = user_input.split(", ")
-
-if len(entered) > 3 or len(entered) <= 0:
-    print("Input is not valid, ensure to follow input guidelines")
-elif len(entered) == 1:
-    print("Defaulting to largest population with city name match ...")
-    input_city = entered[0].replace(' ', '+')
-    search_location()
-elif len(entered) == 2:
-    print("Matching largest population city and within the state/province ...")
-    input_city = entered[0].replace(' ', '+')
-    input_state = entered[1].replace(' ', '+')
-    search_location()
-else:
-    print("Matching all city name, state/province, and country ...")
-    input_city = entered[0].replace(' ', '+')
-    input_state = entered[1].replace(' ', '+')
-    input_country = entered[2].replace(' ', '+')
-    search_location()
+validate_input(get_input())
