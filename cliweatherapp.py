ba1e447d04ab72d1a4e8f9d8d839636cd2715289
akira <zup9su@virginia.edu>
2023-12-31 15:31:35 +0900

    adding in input validation

--- cliweatherapp.py
+++ cliweatherapp.py
@@ -5,44 +5,51 @@ from retry_requests import retry
 from urllib.request import urlopen
 import json
 
-# from open-meteo weather api docs
+# some code from open-meteo weather api docs
+
+def search_location():
+    city_url = f"https://geocoding-api.open-meteo.com/v1/search?name={input_city}&count=1&language=en&format=json"
+    cities = json.loads(urlopen(city_url).read())
+
+    latitude = cities['results'][0]['latitude']
+    longitude = cities['results'][0]['longitude']
+    country = cities['results'][0]['country']
+
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": "temperature_2m"
+    }
+    responses = open_meteo.weather_api(url, params=params)
+
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
 cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 open_meteo = openmeteo_requests.Client(session=retry_session)
 
 user_input = input("What city's weather do you want?\nEnter in form: City, State/Province, Country\n")
-location = user_input.replace(' ', '+')
+entered = user_input.split(", ")
 
-city_url = f"https://geocoding-api.open-meteo.com/v1/search?name={location}&count=1&language=en&format=json"
-cities = json.loads(urlopen(city_url).read())
-
-latitude = cities['results'][0]['latitude']
-longitude = cities['results'][0]['longitude']
-country = cities['results'][0]['country']
-
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-    "latitude": 52.52,
-    "longitude": 13.41,
-    "hourly": "temperature_2m"
-}
-responses = open_meteo.weather_api(url, params=params)
-
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
-
-# hourly = response.Hourly()
-# hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-#
-# hourly_data = {"date": pd.date_range(
-#     start=pd.to_datetime(hourly.Time(), unit="s"),
-#     end=pd.to_datetime(hourly.TimeEnd(), unit="s"),
-#     freq=pd.Timedelta(seconds=hourly.Interval()),
-#     inclusive="left"
-# ), "temperature_2m": hourly_temperature_2m}
-#
-# hourly_dataframe = pd.DataFrame(data=hourly_data)
-# print(hourly_dataframe)
+if len(entered) > 3 or len(entered) <= 0:
+    print("Input is not valid, ensure to follow input guidelines")
+elif len(entered) == 1:
+    print("Defaulting to largest population with city name match")
+    input_city = entered[0].replace(' ', '+')
+    search_location()
+elif len(entered) == 2:
+    print("Matching largest population city and within the state/province")
+    input_city = entered[0].replace(' ', '+')
+    input_state = entered[1].replace(' ', '+')
+    search_location()
+else:
+    print("Matching all city name, state/province, and country")
+    input_city = entered[0].replace(' ', '+')
+    input_state = entered[1].replace(' ', '+')
+    input_country = entered[2].replace(' ', '+')
+    search_location()
