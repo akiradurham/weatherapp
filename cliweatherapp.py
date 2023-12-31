272a191be0f365bbbdb087b6a9727362e612afb3
akira <zup9su@virginia.edu>
2023-12-31 15:43:12 +0900

    added in error checking

--- cliweatherapp.py
+++ cliweatherapp.py
@@ -1,33 +1,37 @@
+import urllib
 import openmeteo_requests
 import requests_cache
 import pandas as pd
 from retry_requests import retry
 from urllib.request import urlopen
 import json
-
 # some code from open-meteo weather api docs
 
+
 def search_location():
-    city_url = f"https://geocoding-api.open-meteo.com/v1/search?name={input_city}&count=1&language=en&format=json"
-    cities = json.loads(urlopen(city_url).read())
+    city_url = f"https://geocoding-api.open-meteo.com/v1/search?name={input_city}&count=10&language=en&format=json"
+    try:
+        cities = json.loads(urlopen(city_url).read())
+        latitude = cities['results'][0]['latitude']
+        longitude = cities['results'][0]['longitude']
+        country = cities['results'][0]['country']
 
-    latitude = cities['results'][0]['latitude']
-    longitude = cities['results'][0]['longitude']
-    country = cities['results'][0]['country']
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "hourly": "temperature_2m"
+        }
+        responses = open_meteo.weather_api(url, params=params)
 
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "hourly": "temperature_2m"
-    }
-    responses = open_meteo.weather_api(url, params=params)
+        response = responses[0]
+        print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
+        print(f"Elevation {response.Elevation()} m asl")
+        print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+        print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    except Exception as e:
+        print("Input is not valid location")
 
-    response = responses[0]
-    print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
 cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
@@ -39,16 +43,16 @@ entered = user_input.split(", ")
 if len(entered) > 3 or len(entered) <= 0:
     print("Input is not valid, ensure to follow input guidelines")
 elif len(entered) == 1:
-    print("Defaulting to largest population with city name match")
+    print("Defaulting to largest population with city name match ...")
     input_city = entered[0].replace(' ', '+')
     search_location()
 elif len(entered) == 2:
-    print("Matching largest population city and within the state/province")
+    print("Matching largest population city and within the state/province ...")
     input_city = entered[0].replace(' ', '+')
     input_state = entered[1].replace(' ', '+')
     search_location()
 else:
-    print("Matching all city name, state/province, and country")
+    print("Matching all city name, state/province, and country ...")
     input_city = entered[0].replace(' ', '+')
     input_state = entered[1].replace(' ', '+')
     input_country = entered[2].replace(' ', '+')
